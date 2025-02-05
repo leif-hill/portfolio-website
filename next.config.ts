ffb9273b2e8d84440be46b3bbf87c0511c997410
Leif Hill <leifhill.business@gmail.com>
2025-02-05 12:18:25 -0500

    fixing config for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-};
-
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  output: "export",
-  assetPrefix: isProd ? "/leif-hill.github.io/" : "",
   images: {
-    unoptimized: true, // Required because GitHub Pages doesn't support Next.js' Image Optimization
+    unoptimized: true, // Optional: Only if you need to disable image optimization
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
