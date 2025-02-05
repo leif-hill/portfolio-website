844eba134a63a359585ea49510a1512aacf8bebe
Leif Hill <leifhill.business@gmail.com>
2025-02-05 13:03:16 -0500

    more mobile fixes

--- src/components/headermobile.tsx
+++ src/components/headermobile.tsx
@@ -28,13 +28,13 @@ const HeaderMobile = ({ show }: HeaderProps) => {
         <a href="#" onClick={navigateToHome}>
           <h1
             className={`${zen.className} bg-white p-0 px-3 inline-block`}
-            style={{ letterSpacing: '0.2em', color: 'var(--header)', transition: 'color var(--transition-duration)', borderRadius: '3px', fontSize: '0.6rem', textAlign: 'center' }}
+            style={{ letterSpacing: '0.2em', color: 'var(--header)', transition: 'color var(--transition-duration)', borderRadius: '3px', fontSize: '0.8rem', textAlign: 'center' }}
           >
             LEIF HILL
           </h1>
           <h1
             className={`${zen.className} p-1 inline-block`}
-            style={{ letterSpacing: '0.1em', color: 'white', fontSize: '0.6rem', textAlign: 'center' }}
+            style={{ letterSpacing: '0.1em', color: 'white', fontSize: '0.8rem', textAlign: 'center' }}
           >
             software engineer
           </h1>
@@ -46,7 +46,7 @@ const HeaderMobile = ({ show }: HeaderProps) => {
             <img
               src={isDarkMode ? '/moon.svg' : '/sun.svg'}
               alt="Toggle theme"
-              style={{ width: '10px', height: '10px' }}
+              style={{ width: '12px', height: '12px' }}
             />
           </button>
         </div>
--- src/components/landingheadermobile.tsx
+++ src/components/landingheadermobile.tsx
@@ -12,10 +12,10 @@ function LandingHeaderMobile() {
     <header id="landing-header" style={{ backgroundColor: '#b881d4', zIndex: 0 }} className="flex justify-center items-center p-4 transition-colors duration-300 relative header-blur" >
       <div className="flex flex-col items-center">
         <a href="#">
-          <h1 className={`${zen.className} bg-white p-0 px-3 inline-block ${isLoaded ? 'fade-in' : ''}`} style={{ letterSpacing: '0.2em', color: '#6757A2', borderRadius: '3px', fontSize: '0.6rem', textAlign: 'center' }}>
+          <h1 className={`${zen.className} bg-white p-0 px-3 inline-block ${isLoaded ? 'fade-in' : ''}`} style={{ letterSpacing: '0.2em', color: '#6757A2', borderRadius: '3px', fontSize: '0.8rem', textAlign: 'center' }}>
             LEIF HILL
           </h1>
-          <h1 className={`${zen.className} p-1 inline-block ${isLoaded ? 'fade-in' : ''}`} style={{ letterSpacing: '0.1em', color: 'white', fontSize: '0.6rem', textShadow: '-2px 1px 5px rgba(62, 62, 62, 0.5)', textAlign: 'center' }}>
+          <h1 className={`${zen.className} p-1 inline-block ${isLoaded ? 'fade-in' : ''}`} style={{ letterSpacing: '0.1em', color: 'white', fontSize: '0.8rem', textShadow: '-2px 1px 5px rgba(62, 62, 62, 0.5)', textAlign: 'center' }}>
             software engineer
           </h1>
         </a>
