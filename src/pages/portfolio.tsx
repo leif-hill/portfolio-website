fb98a60f7328331bff57b51aa4f1623240db3c48
Leif Hill <leifhill.business@gmail.com>
2025-02-05 13:10:50 -0500

    added mobile version of portfolio page

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,12 +1,36 @@
+import { useState, useEffect } from 'react';
 import Header from '../components/header';
+import HeaderMobile from '../components/headermobile';
 import Footer from '../components/footer';
 import PortfolioPage from '../components/portfoliopage';
 
+const isMobile = () => {
+  if (typeof window !== 'undefined') {
+    return /Mobi|Android/i.test(window.navigator.userAgent);
+  }
+  return false;
+};
+
 const Portfolio = () => {
+  const [isMobileDevice, setIsMobileDevice] = useState(false);
+
+  useEffect(() => {
+    setIsMobileDevice(isMobile());
+  }, []);
+
+  if (isMobileDevice) {
+    return (
+      <>
+        <HeaderMobile show={true} />
+        <PortfolioPage />
+        <Footer />
+      </>
+    );
+  }
+
   return (
     <>
       <Header show={true} />
-      <PortfolioPage />
       <PortfolioPage />
       <Footer />
     </>
